7b28e4140a565f589b17a957a2c8f6a599f6e8d4
YUMA-HUB2 <st10474751@rcconnect.edu.za>
2025-06-20 21:51:35 +0000

    Add files via upload
    added javascript

---
+++ index.js
@@ -0,0 +1,13 @@
+<script>
+function showImage(){
+    document.getElementById('heroimg').innerHTML =' <img src="image/bulldog.jpg">'
+}
+
+function showImg(){
+    document.getElementById('heroimg').innerHTML =' <img src="image/chocolate.jpg">'
+}
+
+function showPic(){
+    document.getElementById('heroimg').innerHTML =' <img src="image/cake.jpg">'
+}
+</script>
